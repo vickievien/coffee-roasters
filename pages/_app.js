bf5361afdd93ab7cabaab1d9b1534da114d1ee12
Vickie Vien <vickievien@gmail.com>
2022-11-09 13:00:33 -0800

    :up: update: enable layout across pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>      
+        <Head>
+          <title>Coffee Roasters</title>
+          <meta name="description" content="Coffee roasters webpage" />
+          <link rel="icon" href="/assets/shared/desktop/logo_fav.svg" type="image/svg+xml"/>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+  )
 }
 
 export default MyApp
