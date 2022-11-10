145d7a8168abcb108a1f08bc75a326d51890c2ae
Vickie Vien <vickievien@gmail.com>
2022-11-10 09:23:58 -0800

    :shirt: refactor: update div className for why us component on homepage

--- components/home/Why.js
+++ components/home/Why.js
@@ -10,21 +10,21 @@ const HomeWhyUs = () => {
             <ul className={styles.why_list}>
                 <li className={styles.why_list_item}>
                     <img className={styles.why_icon} src="/assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean icon" />
-                    <div className={styles.why_list_text_containter}>
+                    <div className={styles.why_item_text_container}>
                         <h4>Best quality</h4>
                         <p>Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
                     </div>
                 </li>
                 <li className={styles.why_list_item}>
                     <img className={styles.why_icon} src="/assets/home/desktop/icon-gift.svg" alt="coffee bean icon" />
-                    <div className={styles.why_list_text_containter}>
+                    <div className={styles.why_item_text_container}>
                         <h4>Exclusive benefits</h4>
                         <p>Special offers and swag when you subscribe, including 30% off your first shipment.</p>
                     </div>
                 </li>
                 <li className={styles.why_list_item}>
                     <img className={styles.why_icon} src="/assets/home/desktop/icon-truck.svg" alt="coffee bean icon" />
-                    <div className={styles.why_list_text_containter}>
+                    <div className={styles.why_item_text_container}>
                         <h4>Free shipping</h4>
                         <p>We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.</p>
                     </div>
