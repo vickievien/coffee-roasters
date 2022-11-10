def3ca77e8f57bf79e6afd12a253cd91b826d918
Vickie Vien <vickievien@gmail.com>
2022-11-09 20:13:28 -0800

    :shirt: refactor: refactor classNames for styling

--- components/home/hero.js
+++ components/home/hero.js
@@ -7,7 +7,7 @@ const HomeHero = () => {
             <div className={styles.hero_text_container}>
                 <h2>Great coffee made simple.</h2>
                 <p>Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.</p>
-                <Link href="/create" className="btn">Create your plan</Link>
+                <Link href="/create" className={`btn ${styles.hero_btn}`}>Create your plan</Link>
             </div>
         </section>
     );
