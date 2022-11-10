6291ce21706e35872f3e987f07d9eee6e7db6488
Vickie Vien <vickievien@gmail.com>
2022-11-09 19:45:41 -0800

    :heavy_plus_sign: update: add hero component for home page

---
+++ components/home/hero.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from '../../styles/Home.module.css'
+
+const HomeHero = () => {
+    return (
+        <section className={styles.hero_container}>
+            <div className={styles.hero_text_container}>
+                <h2>Great coffee made simple.</h2>
+                <p>Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.</p>
+                <Link href="/create" className="btn">Create your plan</Link>
+            </div>
+        </section>
+    );
+}
+ 
+export default HomeHero;
