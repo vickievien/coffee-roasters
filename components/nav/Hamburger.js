072436d93fdcccfa5f0b74b0814be3092f8fbdf5
Vickie Vien <vickievien@gmail.com>
2022-11-09 17:34:01 -0800

    :heavy_plus_sign: update: update file path for Hamburger icon. Add new close icon for mobile nav

---
+++ components/nav/Hamburger.js
@@ -0,0 +1,7 @@
+const Hamburger = (props) => {
+    return (
+        <svg onClick={props.toggleMobileNav} className="hamburger" width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M14.5 12a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13zm0-6a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13zm0-6a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13z" fill="#333D4B" fillRule="evenodd"/></svg>
+    );
+}
+ 
+export default Hamburger;
