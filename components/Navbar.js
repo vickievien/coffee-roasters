e37cc3542e2cd482bdfdee703c3002b26756ad60
Vickie Vien <vickievien@gmail.com>
2022-11-09 17:31:15 -0800

    :star: feat: add mobile nav feature

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,30 @@
 import Link from "next/link";
+import { useState } from "react";
+import Hamburger from "./nav/Hamburger";
+import Logo from "./Logo";
+import Close from "./nav/Close";
 
 const Navbar = () => {
+    const [mobileNav, setMobileNav] = useState(false);
+
+    const toggleMobileNav = () => {
+        console.log('clicking');
+        setMobileNav(!mobileNav);
+    }
+
     return ( 
         <nav>
             <div>
-                <Link href="/"><h1>Logo</h1></Link>
+                <Link href="/">
+                    <Logo />
+                </Link>
             </div>
-            <ul>
+            {mobileNav ? 
+            <Close toggleMobileNav={toggleMobileNav} />
+            : 
+            <Hamburger toggleMobileNav={toggleMobileNav} />
+            }
+            <ul className={mobileNav ? 'mobile-nav' : 'default-nav'}>
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/about">About Us</Link></li>
                 <li><Link href="/create">Create Your Plan</Link></li>
