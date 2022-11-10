bb36c4958c31e00e5195e0cd90efa3350783bba8
Vickie Vien <vickievien@gmail.com>
2022-11-09 22:54:41 -0800

    :up: update: add images to Collection on homepage

--- components/home/collection.js
+++ components/home/collection.js
@@ -6,28 +6,28 @@ const HomeCollection = () => {
             <h2>Our Collection</h2>
             <ul className={styles.bean_list}>
                 <li className={styles.bean_list_item}>
-                    <img className={styles.bean_img}/>
+                    <img className={styles.bean_img} src="/assets/home/desktop/image-gran-espresso.png" alt="Gran Espresso coffee beans"/>
                     <div className={styles.bean_text_container}>
                         <h4>Gran Espresso</h4>
                         <p>Light and flavorful blend with cocoa and black pepper for an intense experience</p>
                     </div>
                 </li>
                 <li className={styles.bean_list_item}>
-                    <img className={styles.bean_img}/>
+                    <img className={styles.bean_img} src="/assets/home/desktop/image-planalto.png" alt="Planalto coffee beans"/>
                     <div className={styles.bean_text_container}>
                         <h4>Planalto</h4>
                         <p>Brazilian dark roast with rich and velvety body, and hints of fruits and nuts</p>
                     </div>
                 </li>
                 <li className={styles.bean_list_item}>
-                    <img className={styles.bean_img}/>
+                    <img className={styles.bean_img} src="/assets/home/desktop/image-piccollo.png" alt="Piccollo coffee beans"/>
                     <div className={styles.bean_text_container}>
                         <h4>Piccollo</h4>
                         <p>Mild and smooth blend featuring notes of toasted almond and dried cherry</p>
                     </div>
                 </li>
                 <li className={styles.bean_list_item}>
-                    <img className={styles.bean_img}/>
+                    <img className={styles.bean_img} src="/assets/home/desktop/image-danche.png" alt="Danche coffee beans"/>
                     <div className={styles.bean_text_container}>
                         <h4>Danche</h4>
                         <p>Ethiopian hand-harvested blend densely packed with vibrant fruit notes</p>
