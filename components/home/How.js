86c79859a652d9877c91fa71d00b955314bf835d
Vickie Vien <vickievien@gmail.com>
2022-11-19 10:46:16 +0300

    :up: update: add div to create responsive design for How it works section

--- components/home/How.js
+++ components/home/How.js
@@ -7,16 +7,19 @@ const HomeHow = () => {
             <h3>How it works</h3>
             <ol className={styles.how_list}>
                 <li className={styles.how_list_item}>
+                    <div className={styles.circle}></div>
                     <h4 className={styles.how_list_num}>01</h4>
                     <h4 className={styles.how_list_title}>Pick your coffee</h4>
                     <p>Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.</p>
                 </li>
                 <li className={styles.how_list_item}>
+                    <div className={styles.circle}></div>
                     <h4 className={styles.how_list_num}>02</h4>
                     <h4 className={styles.how_list_title}>Choose the frequency</h4>
                     <p>Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.</p>
                 </li>
                 <li className={styles.how_list_item}>
+                    <div className={styles.circle}></div>
                     <h4 className={styles.how_list_num}>03</h4>
                     <h4 className={styles.how_list_title}>Receive and enjoy!</h4>
                     <p>We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.</p>
