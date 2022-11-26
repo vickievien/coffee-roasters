b3b5c24bce9eceaa9d0864f792cd17cadcb3c1a2
Vickie Vien <vickievien@gmail.com>
2022-11-26 09:31:35 +0300

    :up: update: import About Commit component to About page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import AboutCommit from '../components/about/Commitment';
 import AboutHero from '../components/about/Hero'
 
 const About = () => {
     return (
         <div>
             <AboutHero />
+            <AboutCommit />
         </div>
     );
 }
