a258b98bdda0b9dd2f12667cec5201a19a2f9f7d
Vickie Vien <vickievien@gmail.com>
2022-12-09 16:48:05 +0100

    :up: update: import Quality component to About page

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import AboutCommit from '../components/about/Commitment';
 import AboutHero from '../components/about/Hero'
+import AboutQuality from '../components/about/Quality';
 
 const About = () => {
     return (
         <div>
             <AboutHero />
             <AboutCommit />
+            <AboutQuality />
         </div>
     );
 }
