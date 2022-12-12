1f6f65cdec1e4de92f2ac809e9c36ea2252dc49d
Vickie Vien <vickievien@gmail.com>
2022-12-12 18:18:05 +0100

    :up: update: import How component to Create page

--- pages/create.js
+++ pages/create.js
@@ -1,9 +1,11 @@
 import CreateHero from "../components/create/Hero";
+import CreateHow from "../components/create/How";
 
 const Create = () => {
     return ( 
         <div>
             <CreateHero />
+            <CreateHow />
         </div>
      );
 }
