6686662845a6ed7cc4006a01c86eeac18e33fa93
Vickie Vien <vickievien@gmail.com>
2022-12-13 17:45:09 +0100

    :up: update: import Option component to Create page

--- pages/create.js
+++ pages/create.js
@@ -1,11 +1,13 @@
 import CreateHero from "../components/create/Hero";
 import CreateHow from "../components/create/How";
+import CreateOption from "../components/create/Option";
 
 const Create = () => {
     return ( 
         <div>
             <CreateHero />
             <CreateHow />
+            <CreateOption />
         </div>
      );
 }
