2be66d157cf55b11d15d50b808bed8bc5e1909e5
Vickie Vien <vickievien@gmail.com>
2023-01-09 20:45:59 -0800

    :heavy_plus_sign: update: add Drink page files and component

---
+++ components/drink/Hero.js
@@ -0,0 +1,14 @@
+import styles from '../../styles/Drink.module.css'
+
+const DrinkHero = () => {
+    return (
+        <section className={styles.hero_container}>
+            <div className={styles.hero_text_container}>
+                <h2>Coffee Concoctions</h2>
+                <p>Whether you like your coffee hot or iced, black or sweet, here's a list of some of the most popular drinks for the coffee afficionados.</p>
+            </div>
+        </section>
+    );
+}
+ 
+export default DrinkHero;
---
+++ pages/drink.js
@@ -0,0 +1,10 @@
+import DrinkHero from "../components/drink/Hero";
+
+
+const Drink = () => {
+    return (
+        <DrinkHero />
+    );
+}
+ 
+export default Drink;
