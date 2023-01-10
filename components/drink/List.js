2af72d769519659e6d0f70a4e7ea2a8af8e2a47c
Vickie Vien <vickievien@gmail.com>
2023-01-09 20:53:26 -0800

    :heavy_plus_sign: update: create Drink List component

---
+++ components/drink/List.js
@@ -0,0 +1,19 @@
+import styles from '../../styles/Drink.module.css'
+
+const DrinkList = () => {
+    return (
+        <section className={styles.list_container}>
+            <article>
+                hello 1
+            </article>
+            <article>
+                hello 2
+            </article>
+            <article>
+                hello 3
+            </article>
+        </section>
+    );
+}
+ 
+export default DrinkList;
