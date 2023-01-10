ad71f742ff4b0711945880ff20e664eb492f36b5
Vickie Vien <vickievien@gmail.com>
2023-01-09 21:43:52 -0800

    :up: update: add API call for iced and hot drinks list

--- components/drink/List.js
+++ components/drink/List.js
@@ -1,17 +1,63 @@
+import { useState, useEffect } from 'react';
 import styles from '../../styles/Drink.module.css'
 
 const DrinkList = () => {
+    const [icedDrinks, setIcedDrinks] = useState([])
+    const [hotDrinks, setHotDrinks] = useState([])
+
+    const getIcedDrinks = async() => {
+        const resp = await fetch('https://api.sampleapis.com/coffee/iced')
+        const json = await resp.json();
+        setIcedDrinks(json);
+        console.log(json);
+    }
+
+    const getHotDrinks = async() => {
+        const resp = await fetch('https://api.sampleapis.com/coffee/hot')
+        const json = await resp.json();
+        setHotDrinks(json);
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getIcedDrinks();
+        getHotDrinks();
+      }, []);
+
+    const icedDrinkList = icedDrinks.map(drink => {
+        return (
+            <article key={drink.id}>
+                <div>
+                    <img src={drink.image} alt={drink.title} />
+                </div>
+                <div>
+                    <h2>{drink.title}</h2>
+                    <p>{drink.description}</p>
+                    <p>{drink.ingredients}</p>
+                </div>
+            </article>
+        )
+    })
+
+    const hotDrinkList = hotDrinks.map(drink => {
+        return (
+            <article key={drink.id}>
+                <div>
+                    <img src={drink.image} alt={drink.title} />
+                </div>
+                <div>
+                    <h2>{drink.title}</h2>
+                    <p>{drink.description}</p>
+                    <p>{drink.ingredients}</p>
+                </div>
+            </article>
+        )
+    })
+      
     return (
         <section className={styles.list_container}>
-            <article>
-                hello 1
-            </article>
-            <article>
-                hello 2
-            </article>
-            <article>
-                hello 3
-            </article>
+            {icedDrinkList}
+            {hotDrinkList}
         </section>
     );
 }
