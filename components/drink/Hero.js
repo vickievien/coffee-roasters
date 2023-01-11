220d10daf5815688165a50969b34b354c5e71560
Vickie Vien <vickievien@gmail.com>
2023-01-11 12:01:27 -0800

    :shirt: refactor: update single quote to unicode for deployment

--- components/drink/Hero.js
+++ components/drink/Hero.js
@@ -5,7 +5,7 @@ const DrinkHero = () => {
         <section className={styles.hero_container}>
             <div className={styles.hero_text_container}>
                 <h2>Coffee Concoctions</h2>
-                <p>Whether you like your coffee hot or iced, black or sweet, here's a list of some of the most popular drinks for the coffee afficionados.</p>
+                <p>Whether you like your coffee hot or iced, black or sweet, here&#39;s a list of some of the most popular drinks for the coffee afficionados.</p>
             </div>
         </section>
     );
